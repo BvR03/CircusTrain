e606ccfa2a01377c4c695a171d57ac460b4b1653
BvR03 <thebvrsquad@gmail.com>
2025-05-13 17:04:42 +0200

    Front End
    Added the most scuffed method ever so I didn't have to return to strings to myself the way I used to do it before, hope Onno and Jan are proud.

--- NeoCircusTrain/Form1.Designer.cs
+++ NeoCircusTrain/Form1.Designer.cs
@@ -28,18 +28,320 @@
         /// </summary>
         private void InitializeComponent()
         {
+            LBLCarnivores = new Label();
+            LBLHerbivores = new Label();
+            LBL_CSmall = new Label();
+            LBL_CMedium = new Label();
+            LBL_CLarge = new Label();
+            LBL_HSmall = new Label();
+            LBL_HMedium = new Label();
+            LBL_HLarge = new Label();
+            BTNCSmallIncrease = new Button();
+            BTNCMediumIncrease = new Button();
+            BTNCLargeIncrease = new Button();
+            BTNHSmallIncrease = new Button();
+            BTNHMediumIncrease = new Button();
+            BTNHLargeIncrease = new Button();
+            BTNCSmallDecrease = new Button();
+            BTNCMediumDecrease = new Button();
+            BTNCLargeDecrease = new Button();
+            BTNHSmallDecrease = new Button();
+            BTNHMediumDecrease = new Button();
+            BTNHLargeDecrease = new Button();
+            LBLSmall = new Label();
+            LBLMedium = new Label();
+            LBLLarge = new Label();
+            BTNActCreateTrain = new Button();
             SuspendLayout();
+            // 
+            // LBLCarnivores
+            // 
+            LBLCarnivores.AutoSize = true;
+            LBLCarnivores.Location = new Point(142, 29);
+            LBLCarnivores.Name = "LBLCarnivores";
+            LBLCarnivores.Size = new Size(78, 20);
+            LBLCarnivores.TabIndex = 0;
+            LBLCarnivores.Text = "Carnivores";
+            // 
+            // LBLHerbivores
+            // 
+            LBLHerbivores.AutoSize = true;
+            LBLHerbivores.Location = new Point(370, 29);
+            LBLHerbivores.Name = "LBLHerbivores";
+            LBLHerbivores.Size = new Size(81, 20);
+            LBLHerbivores.TabIndex = 1;
+            LBLHerbivores.Text = "Herbivores";
+            // 
+            // LBL_CSmall
+            // 
+            LBL_CSmall.AutoSize = true;
+            LBL_CSmall.Location = new Point(163, 87);
+            LBL_CSmall.Name = "LBL_CSmall";
+            LBL_CSmall.Size = new Size(17, 20);
+            LBL_CSmall.TabIndex = 2;
+            LBL_CSmall.Text = "0";
+            // 
+            // LBL_CMedium
+            // 
+            LBL_CMedium.AutoSize = true;
+            LBL_CMedium.Location = new Point(163, 127);
+            LBL_CMedium.Name = "LBL_CMedium";
+            LBL_CMedium.Size = new Size(17, 20);
+            LBL_CMedium.TabIndex = 3;
+            LBL_CMedium.Text = "0";
+            // 
+            // LBL_CLarge
+            // 
+            LBL_CLarge.AutoSize = true;
+            LBL_CLarge.Location = new Point(163, 165);
+            LBL_CLarge.Name = "LBL_CLarge";
+            LBL_CLarge.Size = new Size(17, 20);
+            LBL_CLarge.TabIndex = 4;
+            LBL_CLarge.Text = "0";
+            // 
+            // LBL_HSmall
+            // 
+            LBL_HSmall.AutoSize = true;
+            LBL_HSmall.Location = new Point(400, 87);
+            LBL_HSmall.Name = "LBL_HSmall";
+            LBL_HSmall.Size = new Size(17, 20);
+            LBL_HSmall.TabIndex = 5;
+            LBL_HSmall.Text = "0";
+            // 
+            // LBL_HMedium
+            // 
+            LBL_HMedium.AutoSize = true;
+            LBL_HMedium.Location = new Point(400, 127);
+            LBL_HMedium.Name = "LBL_HMedium";
+            LBL_HMedium.Size = new Size(17, 20);
+            LBL_HMedium.TabIndex = 6;
+            LBL_HMedium.Text = "0";
+            // 
+            // LBL_HLarge
+            // 
+            LBL_HLarge.AutoSize = true;
+            LBL_HLarge.Location = new Point(400, 165);
+            LBL_HLarge.Name = "LBL_HLarge";
+            LBL_HLarge.Size = new Size(17, 20);
+            LBL_HLarge.TabIndex = 7;
+            LBL_HLarge.Text = "0";
+            // 
+            // BTNCSmallIncrease
+            // 
+            BTNCSmallIncrease.Location = new Point(202, 83);
+            BTNCSmallIncrease.Name = "BTNCSmallIncrease";
+            BTNCSmallIncrease.Size = new Size(28, 29);
+            BTNCSmallIncrease.TabIndex = 8;
+            BTNCSmallIncrease.Text = ">";
+            BTNCSmallIncrease.UseVisualStyleBackColor = true;
+            BTNCSmallIncrease.Click += BTNCSmallIncrease_Click;
+            // 
+            // BTNCMediumIncrease
+            // 
+            BTNCMediumIncrease.Location = new Point(202, 123);
+            BTNCMediumIncrease.Name = "BTNCMediumIncrease";
+            BTNCMediumIncrease.Size = new Size(28, 29);
+            BTNCMediumIncrease.TabIndex = 9;
+            BTNCMediumIncrease.Text = ">";
+            BTNCMediumIncrease.UseVisualStyleBackColor = true;
+            BTNCMediumIncrease.Click += BTNCMediumIncrease_Click;
+            // 
+            // BTNCLargeIncrease
+            // 
+            BTNCLargeIncrease.Location = new Point(202, 161);
+            BTNCLargeIncrease.Name = "BTNCLargeIncrease";
+            BTNCLargeIncrease.Size = new Size(28, 29);
+            BTNCLargeIncrease.TabIndex = 10;
+            BTNCLargeIncrease.Text = ">";
+            BTNCLargeIncrease.UseVisualStyleBackColor = true;
+            BTNCLargeIncrease.Click += BTNCLargeIncrease_Click;
+            // 
+            // BTNHSmallIncrease
+            // 
+            BTNHSmallIncrease.Location = new Point(437, 83);
+            BTNHSmallIncrease.Name = "BTNHSmallIncrease";
+            BTNHSmallIncrease.Size = new Size(28, 29);
+            BTNHSmallIncrease.TabIndex = 11;
+            BTNHSmallIncrease.Text = ">";
+            BTNHSmallIncrease.UseVisualStyleBackColor = true;
+            BTNHSmallIncrease.Click += BTNHSmallIncrease_Click;
+            // 
+            // BTNHMediumIncrease
+            // 
+            BTNHMediumIncrease.Location = new Point(437, 123);
+            BTNHMediumIncrease.Name = "BTNHMediumIncrease";
+            BTNHMediumIncrease.Size = new Size(28, 29);
+            BTNHMediumIncrease.TabIndex = 12;
+            BTNHMediumIncrease.Text = ">";
+            BTNHMediumIncrease.UseVisualStyleBackColor = true;
+            BTNHMediumIncrease.Click += BTNHMediumIncrease_Click;
+            // 
+            // BTNHLargeIncrease
+            // 
+            BTNHLargeIncrease.Location = new Point(437, 165);
+            BTNHLargeIncrease.Name = "BTNHLargeIncrease";
+            BTNHLargeIncrease.Size = new Size(28, 29);
+            BTNHLargeIncrease.TabIndex = 13;
+            BTNHLargeIncrease.Text = ">";
+            BTNHLargeIncrease.UseVisualStyleBackColor = true;
+            BTNHLargeIncrease.Click += BTNHLargeIncrease_Click;
+            // 
+            // BTNCSmallDecrease
+            // 
+            BTNCSmallDecrease.Location = new Point(119, 83);
+            BTNCSmallDecrease.Name = "BTNCSmallDecrease";
+            BTNCSmallDecrease.Size = new Size(28, 29);
+            BTNCSmallDecrease.TabIndex = 14;
+            BTNCSmallDecrease.Text = "<";
+            BTNCSmallDecrease.UseVisualStyleBackColor = true;
+            BTNCSmallDecrease.Click += BTNCSmallDecrease_Click;
+            // 
+            // BTNCMediumDecrease
+            // 
+            BTNCMediumDecrease.Location = new Point(119, 123);
+            BTNCMediumDecrease.Name = "BTNCMediumDecrease";
+            BTNCMediumDecrease.Size = new Size(28, 29);
+            BTNCMediumDecrease.TabIndex = 15;
+            BTNCMediumDecrease.Text = "<";
+            BTNCMediumDecrease.UseVisualStyleBackColor = true;
+            BTNCMediumDecrease.Click += BTNCMediumDecrease_Click;
+            // 
+            // BTNCLargeDecrease
+            // 
+            BTNCLargeDecrease.Location = new Point(119, 165);
+            BTNCLargeDecrease.Name = "BTNCLargeDecrease";
+            BTNCLargeDecrease.Size = new Size(28, 29);
+            BTNCLargeDecrease.TabIndex = 16;
+            BTNCLargeDecrease.Text = "<";
+            BTNCLargeDecrease.UseVisualStyleBackColor = true;
+            BTNCLargeDecrease.Click += BTNCLargeDecrease_Click;
+            // 
+            // BTNHSmallDecrease
+            // 
+            BTNHSmallDecrease.Location = new Point(354, 83);
+            BTNHSmallDecrease.Name = "BTNHSmallDecrease";
+            BTNHSmallDecrease.Size = new Size(28, 29);
+            BTNHSmallDecrease.TabIndex = 17;
+            BTNHSmallDecrease.Text = "<";
+            BTNHSmallDecrease.UseVisualStyleBackColor = true;
+            BTNHSmallDecrease.Click += BTNHSmallDecrease_Click;
+            // 
+            // BTNHMediumDecrease
+            // 
+            BTNHMediumDecrease.Location = new Point(354, 127);
+            BTNHMediumDecrease.Name = "BTNHMediumDecrease";
+            BTNHMediumDecrease.Size = new Size(28, 29);
+            BTNHMediumDecrease.TabIndex = 18;
+            BTNHMediumDecrease.Text = "<";
+            BTNHMediumDecrease.UseVisualStyleBackColor = true;
+            BTNHMediumDecrease.Click += BTNHMediumDecrease_Click;
+            // 
+            // BTNHLargeDecrease
+            // 
+            BTNHLargeDecrease.Location = new Point(354, 165);
+            BTNHLargeDecrease.Name = "BTNHLargeDecrease";
+            BTNHLargeDecrease.Size = new Size(28, 29);
+            BTNHLargeDecrease.TabIndex = 19;
+            BTNHLargeDecrease.Text = "<";
+            BTNHLargeDecrease.UseVisualStyleBackColor = true;
+            BTNHLargeDecrease.Click += BTNHLargeDecrease_Click;
+            // 
+            // LBLSmall
+            // 
+            LBLSmall.AutoSize = true;
+            LBLSmall.Location = new Point(268, 88);
+            LBLSmall.Name = "LBLSmall";
+            LBLSmall.Size = new Size(46, 20);
+            LBLSmall.TabIndex = 20;
+            LBLSmall.Text = "Small";
+            // 
+            // LBLMedium
+            // 
+            LBLMedium.AutoSize = true;
+            LBLMedium.Location = new Point(268, 131);
+            LBLMedium.Name = "LBLMedium";
+            LBLMedium.Size = new Size(64, 20);
+            LBLMedium.TabIndex = 21;
+            LBLMedium.Text = "Medium";
+            // 
+            // LBLLarge
+            // 
+            LBLLarge.AutoSize = true;
+            LBLLarge.Location = new Point(268, 170);
+            LBLLarge.Name = "LBLLarge";
+            LBLLarge.Size = new Size(46, 20);
+            LBLLarge.TabIndex = 22;
+            LBLLarge.Text = "Large";
+            // 
+            // BTNActCreateTrain
+            // 
+            BTNActCreateTrain.Location = new Point(225, 223);
+            BTNActCreateTrain.Name = "BTNActCreateTrain";
+            BTNActCreateTrain.Size = new Size(141, 29);
+            BTNActCreateTrain.TabIndex = 23;
+            BTNActCreateTrain.Text = "Create Train";
+            BTNActCreateTrain.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1141, 450);
+            Controls.Add(BTNActCreateTrain);
+            Controls.Add(LBLLarge);
+            Controls.Add(LBLMedium);
+            Controls.Add(LBLSmall);
+            Controls.Add(BTNHLargeDecrease);
+            Controls.Add(BTNHMediumDecrease);
+            Controls.Add(BTNHSmallDecrease);
+            Controls.Add(BTNCLargeDecrease);
+            Controls.Add(BTNCMediumDecrease);
+            Controls.Add(BTNCSmallDecrease);
+            Controls.Add(BTNHLargeIncrease);
+            Controls.Add(BTNHMediumIncrease);
+            Controls.Add(BTNHSmallIncrease);
+            Controls.Add(BTNCLargeIncrease);
+            Controls.Add(BTNCMediumIncrease);
+            Controls.Add(BTNCSmallIncrease);
+            Controls.Add(LBL_HLarge);
+            Controls.Add(LBL_HMedium);
+            Controls.Add(LBL_HSmall);
+            Controls.Add(LBL_CLarge);
+            Controls.Add(LBL_CMedium);
+            Controls.Add(LBL_CSmall);
+            Controls.Add(LBLHerbivores);
+            Controls.Add(LBLCarnivores);
             Name = "Form1";
             Text = "Main";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label LBLCarnivores;
+        private Label LBLHerbivores;
+        private Label LBL_CSmall;
+        private Label LBL_CMedium;
+        private Label LBL_CLarge;
+        private Label LBL_HSmall;
+        private Label LBL_HMedium;
+        private Label LBL_HLarge;
+        private Button BTNCSmallIncrease;
+        private Button BTNCMediumIncrease;
+        private Button BTNCLargeIncrease;
+        private Button BTNHSmallIncrease;
+        private Button BTNHMediumIncrease;
+        private Button BTNHLargeIncrease;
+        private Button BTNCSmallDecrease;
+        private Button BTNCMediumDecrease;
+        private Button BTNCLargeDecrease;
+        private Button BTNHSmallDecrease;
+        private Button BTNHMediumDecrease;
+        private Button BTNHLargeDecrease;
+        private Label LBLSmall;
+        private Label LBLMedium;
+        private Label LBLLarge;
+        private Button BTNActCreateTrain;
     }
 }
--- NeoCircusTrain/Form1.cs
+++ NeoCircusTrain/Form1.cs
@@ -9,10 +9,74 @@ namespace NeoCircusTrain
             InitializeComponent();
             //pass
         }
-        
 
+        private void BTNCSmallDecrease_Click(object sender, EventArgs e)
+        {
+            LBL_CSmall.Text = (int.Parse(LBL_CSmall.Text) - 1).ToString();
 
+        }
 
+        private void BTNCSmallIncrease_Click(object sender, EventArgs e)
+        {
+            LBL_CSmall.Text = (int.Parse(LBL_CSmall.Text) + 1).ToString();
 
+        }
+
+        private void BTNCMediumDecrease_Click(object sender, EventArgs e)
+        {
+            LBL_CMedium.Text = (int.Parse(LBL_CMedium.Text) - 1).ToString();
+
+        }
+
+        private void BTNCMediumIncrease_Click(object sender, EventArgs e)
+        {
+            LBL_CMedium.Text = (int.Parse(LBL_CMedium.Text) + 1).ToString();
+        }
+
+        private void BTNCLargeDecrease_Click(object sender, EventArgs e)
+        {
+            LBL_CLarge.Text = (int.Parse(LBL_CLarge.Text) - 1).ToString();
+        }
+
+        private void BTNCLargeIncrease_Click(object sender, EventArgs e)
+        {
+            LBL_CLarge.Text = (int.Parse(LBL_CLarge.Text) + 1).ToString();
+        }
+
+        private void BTNHSmallDecrease_Click(object sender, EventArgs e)
+        {
+            LBL_HSmall.Text = (int.Parse(LBL_HSmall.Text) - 1).ToString();
+
+        }
+
+        private void BTNHSmallIncrease_Click(object sender, EventArgs e)
+        {
+            LBL_HSmall.Text = (int.Parse(LBL_HSmall.Text) + 1).ToString();
+
+        }
+
+        private void BTNHMediumDecrease_Click(object sender, EventArgs e)
+        {
+            LBL_HMedium.Text = (int.Parse(LBL_HMedium.Text) - 1).ToString();
+
+        }
+
+        private void BTNHMediumIncrease_Click(object sender, EventArgs e)
+        {
+            LBL_HMedium.Text = (int.Parse(LBL_HMedium.Text) + 1).ToString();
+
+        }
+
+        private void BTNHLargeDecrease_Click(object sender, EventArgs e)
+        {
+            LBL_HLarge.Text = (int.Parse(LBL_HLarge.Text) - 1).ToString();
+
+        }
+
+        private void BTNHLargeIncrease_Click(object sender, EventArgs e)
+        {
+            LBL_HLarge.Text = (int.Parse(LBL_HLarge.Text) + 1).ToString();
+
+        }
     }
 }
